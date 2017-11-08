87b71fe261ae123f9c65994956844a227b49bc8c
Fred Clinton Ahanogbe <fred.ahanogbe@gmail.com>
2017-11-08 11:21:30 +0000

    invoice history

--- resources/views/invoicing/invoiceTable.blade.php
+++ resources/views/invoicing/invoiceTable.blade.php
@@ -45,8 +45,8 @@
             <td>{{$i->vessel_name}}</td>
             <td>{{$i->bill_item}}</td>
             <td>{{$i->vat}}</td>
-            <td>{{$i->created_at}}</td>
             <td>Unit Price: {{$i->unit_price }} / Quanity: {{$i->quantity}} / Total Cost: {{$i->actual_cost}}</td>
+            <td>{{$i->created_at}}</td>
             <td class="del">
                 <Button value="{{$i->invoice_id}}" class="del-class"><i class="fa fa-trash-o"></i></Button>
             </td>
